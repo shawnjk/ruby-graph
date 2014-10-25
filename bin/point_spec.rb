ddf96a1488de7719dd652c5593ce2ce7b8aa3ecb
Shawn <sjkim_09@yahoo.com>
2014-10-25 19:12:27 -0400

    Adding more unit tests, redefining slope for vertical and horizontal lines

--- bin/point_spec.rb
+++ bin/point_spec.rb
@@ -39,27 +39,41 @@ end
 RSpec.describe Line do
 
   describe '::calculate_with_points' do
-    context 'given points (10, 4) and (3, 4)' do
-      it 'calculates line y = 4' do
+    context 'given (10, 4) and (3, 4)' do
+      before(:each) do
         point_a = Point.new(10, 4)
         point_b = Point.new(3, 4)
-        actual = Line.calculate_with_points(point_a, point_b)
+        @actual = Line.calculate_with_points(point_a, point_b)
+      end 
+      it 'calculates line y = 4 (using ::calculate_with_intercepts)' do
         expected = Line.calculate_with_intercepts(nil, Point.new(0, 4))
-        expect(actual).to eq(expected)
+        expect(@actual).to eq(expected)
+      end
+
+      it 'calculates line y = 4 (using ::calculate_with_slope_and_point)' do
+        expected = Line.calculate_with_slope_and_point(0, Point.new(0, 4))
+        expect(@actual).to eq(expected)
       end
     end
 
-    context 'given points (3, 10) and (3, 0)' do
-      it 'calculates line x = 3' do
+    context 'given (3, 10) and (3, 0)' do
+      before(:each) do
         point_a = Point.new(3, 10)
         point_b = Point.new(3, 0)
-        actual = Line.calculate_with_points(point_a, point_b)
+        @actual = Line.calculate_with_points(point_a, point_b)
+      end
+      it 'calculates line x = 3 (using ::calculate_with_intercepts)' do
         expected = Line.calculate_with_intercepts(Point.new(3, 0), nil)
-        expect(actual).to eq(expected)
+        expect(@actual).to eq(expected)
+      end
+
+      it 'calculates line x = 3 (using ::calculate_with_slope_and_point)' do
+        expected = Line.calculate_with_slope_and_point(nil, Point.new(3, 0))
+        expect(@actual).to eq(expected)
       end
     end
 
-    context 'given points (3, 32) and (4, 50)' do
+    context 'given (3, 32) and (4, 50)' do
       it 'calculates the same line independent of parameter order' do
         point_a = Point.new(3, 32)
         point_b = Point.new(4, 50)
@@ -69,25 +83,22 @@ RSpec.describe Line do
       end
     end
 
-    context 'given points (32, 20) and (300, 26)' do 
-      it 'calculates line y = 3/134x + 1292/67 (using ::calculate_with_intercepts)' do
+    context 'given (32, 20) and (300, 26)' do 
+      before(:each) do 
         point_a = Point.new(32, 20)
         point_b = Point.new(300, 26)
+        @actual = Line.calculate_with_points(point_a, point_b)
+      end
+      it 'calculates line y = 3/134x + 1292/67 (using ::calculate_with_intercepts)' do
         expected_intercept_x = Point.new((-1292.0/67) * (134.0/3), 0)
         expected_intercept_y = Point.new(0, 1292.0/67)
-        actual = Line.calculate_with_points(point_a, point_b)
         expected = Line.calculate_with_intercepts(expected_intercept_x, expected_intercept_y)
-        expect(actual).to eq(expected)
+        expect(@actual).to eq(expected)
       end
-    end
 
-    context 'given points (32, 20) and (300, 26)' do 
-      it 'calculates line y = 3/134x + 1292/67 (using ::calculate_with_slope_and_point)' do
-        point_a = Point.new(32, 20)
-        point_b = Point.new(300, 26)
-        actual = Line.calculate_with_points(point_a, point_b)
-        expected = Line.calculate_with_slope_and_point(3.0/134, point_a)
-        expect(actual).to eq(expected)
+      it 'calculates y = 3/134x + 1292/67 (using ::calculate_with_slope_and_point)' do
+        expected = Line.calculate_with_slope_and_point(3.0/134, Point.new(0, 1292.0/67))
+        expect(@actual).to eq(expected)
       end
     end
 
@@ -104,40 +115,133 @@ RSpec.describe Line do
   describe '#intersection' do
     context '2 horizontally parallel lines' do
       it 'raises ArgumentError' do
-        
+        line_a_point_a = Point.new(2, 50)
+        line_a_point_b = Point.new(100, 50)
+        line_b_point_a = Point.new(10, 400)
+        line_b_point_b = Point.new(1000, 400)
+        line_a = Line.calculate_with_points(line_a_point_a, line_a_point_b)
+        line_b = Line.calculate_with_points(line_b_point_a, line_b_point_b)
+        expect { line_a.intersection(line_b) }.to raise_error(ArgumentError)
       end
     end
 
     context '2 vertically parallel lines' do
       it 'raises ArgumentError' do
+        line_a_point_a = Point.new(2, 50)
+        line_a_point_b = Point.new(2, -36)
+        line_b_point_a = Point.new(400, 39)
+        line_b_point_b = Point.new(400, -1000)
+        line_a = Line.calculate_with_points(line_a_point_a, line_a_point_b)
+        line_b = Line.calculate_with_points(line_b_point_a, line_b_point_b)
+        expect { line_a.intersection(line_b) }.to raise_error(ArgumentError)
+      end
     end
 
     context 'given lines y = 4 and x = 30' do 
-      it 'returns point (30, 4)' do
+      before(:each) do 
+        @line_a = Line.calculate_with_intercepts(Point.new(30, 0), nil)
+        @line_b = Line.calculate_with_intercepts(nil, Point.new(0, 4))
+        @expected = Point.new(30, 4)
+      end
 
+      it 'returns point (30, 4)' do
+        actual = @line_a.intersection(@line_b)
+        expect(actual).to eq(@expected)
+      end
+
+      it 'returns point (30, 4) (commutative)' do
+        actual = @line_b.intersection(@line_a)
+        expect(actual).to eq (@expected)
       end
     end
 
     context 'lines y = 4 and y = 15x + 32' do
-      it 'returns point (-28/15, 4)' do
+      before(:each) do
+        @line_a = Line.calculate_with_intercepts(nil, Point.new(0, 4))
+        @line_b = Line.calculate_with_slope_and_point(15, Point.new(0, 32))
+        @expected = Point.new(-28.0/15, 4)
+      end
 
+      it 'returns point (-28/15, 4)' do
+        actual = @line_a.intersection(@line_b) 
+        expect(actual).to eq(@expected)
+      end
+
+      it 'returns point(-28/15, 4) (commutative)' do
+        actual = @line_b.intersection(@line_a) 
+        expect(actual).to eq(@expected)
       end
     end
 
     context 'x = 78 and y = 15x + 32' do
+      before(:each) do
+        @line_a = Line.calculate_with_intercepts(Point.new(78, 0), nil)
+        @line_b = Line.calculate_with_slope_and_point(15, Point.new(0, 32))
+        @expected = Point.new(78, 1202)
+      end
+
       it 'returns point (78, 1202)' do
-        
+        actual = @line_a.intersection(@line_b)
+        expect(actual).to eq(@expected)
+      end
+
+      it 'returns point (78, 1202) (commutative)' do
+        actual = @line_b.intersection(@line_a)
+        expect(actual).to eq(@expected)
       end
     end
 
-    context '2 slanted lines' do
-      it 'calculates intersection' do
-      
+    context 'y = 5x - 20 and y = 12x+32' do
+      it 'returns point (-52/7, -400/7)' do
+        line_a = Line.calculate_with_slope_and_point(5, Point.new(0, -20))
+        line_b = Line.calculate_with_slope_and_point(12, Point.new(0, 32))
+        actual = line_a.intersection(line_b)
+        expected = Point.new(-52.0/7, -400.0/7)
+        expect(actual).to eq(expected) 
       end
     end
   end
 
+  describe '#distance_to_point' do
+    context 'y = 35x + 100 and point (15, 30)' do
+      it 'returns distance 595/sqrt(1226)' do
+        line = Line.calculate_with_slope_and_point(35, Point.new(0, 100))
+        point = Point.new(15, 30) 
+        actual = line.distance_to_point(point)
+        expected = 595/Math.sqrt(1226)
+        expect(actual).to eq(expected)
+      end
+    end
+    
+    context 'y = 20x + 94 and point (16, 414)' do
+      it 'returns distance 0' do
+        line = Line.calculate_with_slope_and_point(20, Point.new(0, 94))
+        point = Point.new(16, 414)
+        actual = line.distance_to_point(point)
+        expected = 0
+        expect(actual).to eq(expected)
+      end 
+    end
+
+    context 'y = 160 and point (10, 1000)' do
+      it 'returns distance 840' do
+        line = Line.calculate_with_intercepts(nil, Point.new(0, 160))
+        point = Point.new(10, 1000)
+        actual = line.distance_to_point(point)
+        expected = 840
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context 'x = 14 and point (10, 24)' do
+      it 'returns distance 4' do
+        line = Line.calculate_with_intercepts(Point.new(14, 0), nil)
+        point = Point.new(10, 24)
+        actual = line.distance_to_point(point)
+        expected = 4
+        expect(actual).to eq(expected)
+      end
+    end
+  end
 end
-
-
 
--- lib/Graph/line.rb
+++ lib/Graph/line.rb
@@ -1,22 +1,18 @@
 module Graph
   class Line
-    attr_accessor :x_intercept, :y_intercept
+    attr_reader :x_intercept, :y_intercept
 
     def self.calculate_with_points(_point_a, _point_b)
-      ArgumentError.new("Points must be distinct") if _point_a == _point_b
+      raise ArgumentError, "Points must be distinct" if _point_a == _point_b
       line = Line.new
       slope = _calculate_slope(_point_a, _point_b)
       line.instance_eval { _set_slope(slope) }
       if slope == 0.0
-        if (_point_a.x == _point_b.x)
+        line.x_intercept = nil
+        line.y_intercept = Point.new(0, _point_a.y) 
+      elsif slope.nil?
           line.x_intercept = Point.new(_point_a.x, 0.0)
           line.y_intercept = nil
-        elsif (_point_a.y == _point_b.y)
-          line.x_intercept = nil
-          line.y_intercept = Point.new(0, _point_a.y) 
-        else
-          ArgumentError.new("Cannot calculate a line given #{_point_a.to_s} and #{_point_b.to_s}")
-        end
       else
         line.y_intercept = _calculate_y_intercept(slope, _point_a)
         line.x_intercept = _calculate_x_intercept(slope, _point_a)
@@ -34,7 +30,7 @@ module Graph
     end
 
     def self.calculate_with_slope_and_point(_slope, _point)
-      ArgumentError.new("Cannot calculate a line given a 0 slope and a 1 point") if _slope == 0
+      #raise ArgumentError, "Cannot calculate a line given a 0 slope and a 1 point" if _slope == 0
       line = Line.new
       line.instance_eval { _set_slope(_slope) }
       line.x_intercept = _calculate_x_intercept(_slope, _point)
@@ -59,7 +55,7 @@ module Graph
     def intersection(_line)
       if _line.x_intercept.nil?
         if self.x_intercept.nil?
-          raise ArgumentError "Intersection cannot be found -- lines are parallel"
+          raise ArgumentError, "Intersection cannot be found -- lines are parallel"
         elsif self.y_intercept.nil?
           return Point.new(self.x_intercept.x, _line.y_intercept.y) 
         else
@@ -68,18 +64,36 @@ module Graph
         end
       elsif _line.y_intercept.nil?
         if self.y_intercept.nil?
-          raise ArgumentError "Intersection cannot be found -- lines are parallel"
+          raise ArgumentError, "Intersection cannot be found -- lines are parallel"
         elsif self.x_intercept.nil?
           return Point.new(_line.x_intercept.x, self.y_intercept.y)
         else
           intersect_y = self.slope * _line.x_intercept.x + self.y_intercept.y
           return Point.new(_line.x_intercept.x, intersect_y)
         end
+      elsif self.y_intercept.nil?
+        if _line.y_intercept.nil?
+          raise ArgumentError, "Intersection cannot be found -- lines are parallel"
+        elsif _line.x_intercept.nil?
+          return Point.new(self.x_intercept.x, _line.y_intercept.y)
+        else
+          intersect_y = _line.slope * self.x_intercept.x + _line.y_intercept.y
+          return Point.new(self.x_intercept.x, intersect_y)
+        end
+      elsif self.x_intercept.nil?
+        if _line.x_intercept.nil?
+          raise ArgumentError, "Intersection cannot be found -- lines are parallel"
+        elsif _line.y_intercept.nil?
+          return Point.new(_line.x_intercept.x, self.y_intercept.y) 
+        else
+          intersect_x = (self.y_intercept.y - _line.y_intercept.y) / _line.slope
+          return Point.new(intersect_x, self.y_intercept.y)
+        end
       end
       #calculate intersections using mx - y = -b
       determinant = (self.slope * -1) - (_line.slope * -1)
       determinant_x = (self.y_intercept.y) - (_line.y_intercept.y)
-      determinant_y = (self.slope * _line.y_intercept.y * -1) - (_line.slope * _self.y_intercept.y * -1)
+      determinant_y = (self.slope * _line.y_intercept.y * -1) - (_line.slope * self.y_intercept.y * -1)
       intersect_x = determinant_x/determinant
       intersect_y = determinant_y/determinant
       Point.new(intersect_x, intersect_y)
@@ -91,22 +105,57 @@ module Graph
 
     def self._calculate_slope(_point_a, _point_b)
       difference = _point_a - _point_b
-      difference.x == 0.0 ? 0.0  : difference.y / difference.x
+      if difference.x == 0.0
+        return nil
+      elsif difference.y == 0.0
+        return 0.0
+      else 
+        difference.y / difference.x
+      end
     end
 
     def self._calculate_y_intercept(_slope, _point)
+      return nil if _slope.nil? 
       y_intercept = _point.y - (_slope * _point.x)
       Point.new(0, y_intercept)
     end
 
     def self._calculate_x_intercept(_slope, _point)
+      if _slope.nil?
+        return Point.new(_point.x, 0)
+      elsif _slope == 0
+        return nil
+      end
       x_intercept = ((-1 * _point.y) / _slope) + _point.x
       Point.new(x_intercept, 0)
     end
 
     def _set_slope(_slope)
-      @_slope = Float _slope 
+      if _slope.nil?
+        @_slope = nil
+      else
+        @_slope = (Float _slope).round(10)
+      end
     end
+
+    def x_intercept=(_x_intercept)
+      if _x_intercept.nil?
+        @x_intercept = nil
+      elsif
+        x_intercept_rounded = Point.new((Float _x_intercept.x).round(10), 0)
+        @x_intercept = x_intercept_rounded
+      end
+    end
+
+    def y_intercept=(_y_intercept)
+      if _y_intercept.nil?
+        @y_intercept = nil
+      elsif
+        y_intercept_rounded = Point.new(0, (Float _y_intercept.y).round(10))
+        @y_intercept = y_intercept_rounded
+      end
+    end
+
 
     def slope
       @_slope
